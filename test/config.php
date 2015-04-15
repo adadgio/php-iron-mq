7030cb053e0ce01c1b57b0f361bbd38d3fcf7bc0
Romain BRUCKERT <romain.bruckert.esc@gmail.com>
2015-04-15 11:19:54 +0200

    Config test

--- test/config.php
+++ test/config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-	'apiKey' => "qgDQ72KsraISmD3x7GB49lIpvR0",
-	'projectKey' => "552e20aacc927c0006000043",
+	'apiKey' => "IRONMQ_APIKEY",
+	'projectKey' => "IRONMQ_PROJECID",
 );
 ?>
